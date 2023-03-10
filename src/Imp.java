b7c0e13fb5b6ac74e0fa8849b2f206f862579d4d
visal sak <113172768+sakvisal@users.noreply.github.com>
2023-03-10 10:52:34 +0700

    Add new class

---
+++ src/Imp.java
@@ -0,0 +1,22 @@
+public class Imp implements Calculator {
+
+    @Override
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int mul(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return 0;
+    }
+}
